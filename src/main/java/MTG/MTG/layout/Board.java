8c4850b839a7c715070645191c65fdc91ce481e0
lug4ru <lug4ru@gmail.com>
2022-01-12 10:46:28 +0100

    w/o DB|AProps

---
+++ src/main/java/MTG/MTG/layout/Board.java
@@ -0,0 +1,27 @@
+package MTG.MTG.layout;
+
+import MTG.MTG.service.Broadcaster;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.dnd.DropTarget;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@StyleSheet("../CSS/stylesheet.css")
+public class Board extends VerticalLayout implements DropTarget<VerticalLayout>{
+
+    @Autowired
+    public Board() {
+        this.setMaxWidth("62%");
+        this.addClassName("Board");
+        this.setHeight(94 + "px");
+        setActive(true);
+
+        addDropListener(event->{
+            MainView.boardId = this.getId().get();
+            Broadcaster.broadcast(String.valueOf(event.getDragSourceComponent().get()));
+            this.add(event.getDragSourceComponent().get());
+            this.remove(event.getDragSourceComponent().get());
+        });
+    }
+
+}
---
+++ src/main/java/MTG/MTG/layout/ChatLayout.java
@@ -0,0 +1,73 @@
+package MTG.MTG.layout;
+
+import MTG.MTG.domain.ChatMessage;
+import MTG.MTG.domain.MessageList;
+import MTG.MTG.domain.Nutzer;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
+
+@StyleSheet("../CSS/stylesheet.css")
+@Push
+@Route("/chat")
+public class ChatLayout extends VerticalLayout {
+
+
+    private final UnicastProcessor<ChatMessage> publisher;
+    private final Flux<ChatMessage> messages;
+    private final MessageList messageList = new MessageList();
+
+
+    public ChatLayout(UnicastProcessor<ChatMessage> publisher, Flux<ChatMessage> messages) {
+        this.publisher = publisher;
+        this.messages = messages;
+        this.addClassName("Chat-Layout");
+        this.setMaxWidth(250 + "px");
+        add(getMessageList());
+        add(getInputLayout());
+    }
+    private Component getInputLayout() {
+        HorizontalLayout inputLayout = new HorizontalLayout();
+        TextField messageField = new TextField();
+        messageField.setMaxWidth(150 + "px");
+        Button sendButton = new Button("Send");
+        sendButton.addClassName("sendbutton");
+        sendButton.setMaxWidth("10%");
+        sendButton.getStyle().set("height", 35 + "px");
+        sendButton.getStyle().set("bottom", 0 + "px");
+        inputLayout.getStyle().set("bottom", 0 + "px");
+
+        inputLayout.add(messageField, sendButton);
+        expand(messageField);
+        sendButton.addClickListener(event-> {
+            publisher.onNext(new ChatMessage(VaadinSession.getCurrent().getAttribute(Nutzer.class).getUsername(),
+                    messageField.getValue()));
+            messageField.clear();
+            messageField.focus();
+
+        });
+        return inputLayout;
+    }
+
+    private MessageList getMessageList() {
+        messageList.addClassName("message-list");
+        expand(messageList);
+        messages.subscribe(message-> {
+            getUI().ifPresent(ui -> ui.access(()-> {
+                messageList.add(new Paragraph(
+                        message.getTime() + " - " + message.getFrom() + ": " + message.getMessage()
+                ));
+            }));
+        });
+        return messageList;
+    }
+}
